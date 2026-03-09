e5514b63e3dee85c445d283a36db16d7e079c1b2
teodorglisic <teodor.glisic13@icloud.com>
2026-03-09 12:04:54 +0100

    First functional build of the /deliveryRuleManager POST-method

--- src/main/java/com/example/droolsengine/LogisticsController.java
+++ src/main/java/com/example/droolsengine/LogisticsController.java
@@ -1,7 +1,11 @@
 package com.example.droolsengine;
 
+import org.apache.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.support.SessionStatus;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,10 +14,23 @@ import java.util.Map;
 
 public class LogisticsController {
 
+     private final LogisticsService logisticsService;
+
+    public LogisticsController(LogisticsService logisticsService) {
+        this.logisticsService = logisticsService;
+    }
+
     @GetMapping(path = "/ping", produces = "application/json")
     public HashMap<String, Boolean> ping() {
         HashMap<String, Boolean> result = new HashMap<>();
         result.put("ping", true);
         return result;
+    }
+
+    @PostMapping(path = "/deliveryRuleManager", produces = "application/json")
+    public Logistics deliveryRuleCall(@RequestBody Logistics logistics, SessionStatus sessionStatus) {
+        logisticsService.logisticDecisionManager(logistics);
+
+        return logistics;
     }
 }
